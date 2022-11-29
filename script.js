27ea0b3817f40ff2346ffd95babca9ac4abce279
Arc-Data <arclight04@gmail.com>
2022-11-29 16:47:08 +0800

    adds backspace functionality

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const currNumber = document.querySelector('#currNumber');
 const previousNumber = document.querySelector('.prev');
 const pending = document.querySelector('.pending');
-
+const clearButton = document.querySelector('#clearButton');
+const backspaceButton = document.querySelector('#backSpaceButton');
 const numbers = document.querySelectorAll('.numbers');
 
-numbers.forEach(number => {
-	number.addEventListener('click', appendDigit)
-})
-
 function appendDigit(e) {
-	currNumber.textContent += e.currentTarget.textContent;
+	const digit = e.currentTarget.id;
+
+	if(currNumber.textContent == '0') {
+		currNumber.textContent = digit;
+	} else {
+		currNumber.textContent += digit;
+	}
 }
 
 
@@ -22,7 +25,29 @@ function inputNumber(event) {
 	}
 }
 
-currNumber.textContent = ''
+clearButton.addEventListener('click', () => {
+	currNumber.textContent = '0';
+	previousNumber.textContent = '';
+})
+
+backspaceButton.addEventListener('click', () => {
+	const digits = currNumber.textContent;
+	if(digits == '0') return;
+	currNumber.textContent = digits.slice(0, digits.length - 1);
+	if(!currNumber.textContent) {
+		currNumber.textContent = '0';
+	}
+})
+
+numbers.forEach(number => {
+	number.addEventListener('click', appendDigit)
+})
+
+
+
+
+
 
 window.addEventListener('keypress', inputNumber);
 
+currNumber.textContent = '0';
