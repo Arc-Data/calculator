81488f8efc6e3e1ba8ed97124581ec48e61399bb
Arc-Data <arclight04@gmail.com>
2022-11-29 16:28:33 +0800

    digit input and keyboard support for numbers

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Im working")
+const currNumber = document.querySelector('#currNumber');
+const previousNumber = document.querySelector('.prev');
+const pending = document.querySelector('.pending');
+
+const numbers = document.querySelectorAll('.numbers');
+
+numbers.forEach(number => {
+	number.addEventListener('click', appendDigit)
+})
+
+function appendDigit(e) {
+	currNumber.textContent += e.currentTarget.textContent;
+}
+
+
+function inputNumber(event) {
+	event.preventDefault();
+	for(let i = 48; i < 58; i++) {
+		if(event.keyCode === i) {
+			currNumber.textContent += String.fromCharCode(event.keyCode)
+		}	
+	}
+}
+
+currNumber.textContent = ''
+
+window.addEventListener('keypress', inputNumber);
+
