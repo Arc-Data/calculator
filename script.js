c84db1590577c07f5cd68bfb00ce845fcb19ff93
Arc-Data <arclight04@gmail.com>
2022-11-30 14:01:19 +0800

    adds backspace functionality and keyboard support

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const result = document.querySelector('.resultScreen');
 const operationScreen = document.querySelector('.operationScreen');
 const numbers = document.querySelectorAll('.numbers');
+const backspaceButton = document.querySelector('#backspace');
 
+bool isDecimal = false;
 
+function backSpace() {
+	if(result.textContent == '0') return;
+	const text = result.textContent;
 
+	if (result.textContent.length == 1) {
+		result.textContent = '0';
+
+	} else {
+		result.textContent  = text.slice(0, text.length - 1);
+	}
+}
 
 function handleInput(e) {
 	if(e.key >= 0 && e.key <= 9) appendDigit(e.key)
+	else if(e.key === 'Backspace') backSpace();
 }
 
 function appendDigit(digit) {
@@ -18,6 +31,8 @@ function appendDigit(digit) {
 	result.textContent += digit;
 }
 
+
+backspaceButton.addEventListener('click', backSpace);
 
 window.addEventListener('keydown', handleInput);
 
