7782581176b88ca425b04750afe8f998f4a359de
Arc-Data <arclight04@gmail.com>
2022-11-30 14:32:51 +0800

    complete addition functionality

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
-const result = document.querySelector('.resultScreen');
+const resultScreen = document.querySelector('.resultScreen');
 const operationScreen = document.querySelector('.operationScreen');
 const numbers = document.querySelectorAll('.numbers');
 const backspaceButton = document.querySelector('#backspace');
+const operators = document.querySelectorAll('.op')
+const equalsButton = document.querySelector('#equals')
 
-bool isDecimal = false;
+let isDecimal = false;
+let num1 = '';
+let num2 = '';
+let currentOperation = '';
+
+function checkOperation(operation) {
+	if(currentOperation) {
+		return evaluate()
+	};
+	num1 = resultScreen.textContent;
+	resultScreen.textContent = 0;
+	operationScreen.textContent = `${num1} ${operation}`
+	currentOperation = operation;
+}
+
+function evaluate() {
+	if(!currentOperation) return;
+	num1 = Number(num1);
+	num2 = Number(resultScreen.textContent);
+	operate(num1, num2);
+}
+
+function operate(a, b) {
+	let text = '';
+	switch(currentOperation) {
+		case '+':
+			text = add(a, b);
+			break;
+
+	}
+
+	operationScreen.textContent = `${a} ${currentOperation} ${b}`
+	resultScreen.textContent = text;
+	currentOperation = ''
+}
+
+function add(a, b) {
+	return a + b;
+}
 
 function backSpace() {
-	if(result.textContent == '0') return;
-	const text = result.textContent;
+	if(resultScreen.textContent == '0') return;
+	const text = resultScreen.textContent;
 
-	if (result.textContent.length == 1) {
-		result.textContent = '0';
-
+	if (resultScreen.textContent.length == 1) {
+		resultScreen.textContent = '0';
 	} else {
-		result.textContent  = text.slice(0, text.length - 1);
+		resultScreen.textContent  = text.slice(0, text.length - 1);
 	}
 }
 
@@ -23,14 +62,20 @@ function handleInput(e) {
 }
 
 function appendDigit(digit) {
-	if(result.textContent == '0') {
-		result.textContent = digit;
+	if(resultScreen.textContent == '0') {
+		resultScreen.textContent = digit;
 		return;
 	}
 
-	result.textContent += digit;
+	resultScreen.textContent += digit;
 }
 
+equalsButton.addEventListener('click', evaluate);
+
+
+operators.forEach(operator => {
+	operator.addEventListener('click', () => checkOperation(operator.textContent))
+})
 
 backspaceButton.addEventListener('click', backSpace);
 
